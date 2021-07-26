9afd1f3971a8d81186894e9749f5740d6caf03b6
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-07-26 20:52:22 -0300

    Criar header e fazer ajustes

--- src/app/pages/lets-do-math/lets-do-math.module.ts
+++ src/app/pages/lets-do-math/lets-do-math.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LetsDoMathComponent } from './lets-do-math.component';
 import { LetsDoMathRoutingModule } from './lets-do-math-routing.module';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
   declarations: [
-    LetsDoMathComponent
+    LetsDoMathComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
